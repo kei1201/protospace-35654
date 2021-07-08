9f420bd43935255ae90a91f93276e6f9d0ec0674
kei1201 <k16180339@icloud.com>
2021-07-08 10:19:35 +0900

    修正完了

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :new, :create, :edit]
+  before_action :authenticate_user!, only: [:edit, :new ]
   before_action :move_to_index, except: [:index, :show]
 
   def index
